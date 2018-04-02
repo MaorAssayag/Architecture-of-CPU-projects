3f4064dcada435beb917d40d03e4e8d3ac8ed794
Maor Assayag <maorassayag16@gmail.com>
2018-04-02 13:04:05 +0300

    update

--- Design a basic ALU/ADD.vhd
+++ Design a basic ALU/ADD.vhd
@@ -13,8 +13,7 @@
 -- libraries decleration
 library ieee;
 use ieee.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
-use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
  -- entity Definition
 entity adder is
@@ -29,8 +28,7 @@ end adder;
 
  -- Architecture Definition
 architecture gate_level of adder is
-component full_adder
-    port (
+component full_adder is port (
           a :    in std_logic;
           b :    in std_logic;
           Cin :  in std_logic;
@@ -42,13 +40,13 @@ end component;
 signal tmp : std_logic_vector (N-1 downto 0);
 begin
 ----------------------------------------
-    stage_0 : full_adder port map (A(0) => a, B(0) =>b, '0'=>Cin, SUM(0)=>sum, tmp(0)=>Cout);
+    stage_0 : full_adder port map (A(0) , B(0) , '0', SUM(0), tmp(0));
 
     Array_Of_full_adders: for i in 1 to (N-2) generate
-        stage_i : full_adder port map (A(i) => a, B(i) =>b, tmp(i-1)=>Cin, SUM(i)=>sum, tmp(i)=>Cout)
+        stage_i : full_adder port map (A(i) , B(i) , tmp(i-1), SUM(i), tmp(i));
     end generate;
 
-    stage_final : full_adder port map (A(N-1) => a, B(N-1) =>b, tmp(N-2)=>Cin, SUM(N-1)=>sum, CARRY=>Cout);
+    stage_final : full_adder port map (A(N-1) , B(N-1) , tmp(N-2), SUM(N-1), CARRY);
 ----------------------------------------
 end gate_level;
 
--- Design a basic ALU/TestBench_full_adder.vhd
+++ Design a basic ALU/TestBench_full_adder.vhd
@@ -95,4 +95,4 @@ begin
 
   end process stim;
 ----------------------------------------
-end Testbench_full_adder;
+end behavior;
