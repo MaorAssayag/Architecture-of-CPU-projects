dab9589448ca5f43de60f2b4caa15b77b0bbac8c
refael <rapooil120@gmail.com>
2018-04-05 18:04:33 +0300

    shr

--- Design a basic ALU/VHDL/SHR.vhd
+++ Design a basic ALU/VHDL/SHR.vhd
@@ -34,17 +34,22 @@ architecture gate_level of SHR is
       );
   end component;
 
-
-signal tmp : std_logic_vector (N-1 downto 0) := A;
-signal tmp2 : integer := conv_integer(B);
+--
+-- signal tmp : std_logic_vector (N-1 downto 0) := "10100101";
+-- signal tmp2 : integer := conv_integer(B);
+-- signal tmp3 : std_logic_vector (N-1 downto 0) ;
 begin
-----------------------------------------
--- Shift <= std_logic_vector(A srl conv_integer(B));
-loopforshift: for i in 0 to tmp2 generate
-    stage_i : SHR_ONE port map (tmp,tmp);
-end generate;
-Shift <= tmp;
-
+  process(A,B)
+        variable tmp : integer := conv_integer(B);
+        variable tmp2 : std_logic_vector(N-1 downto 0) :=A;
+begin
+-- ----------------------------------------
+-- -- Shift <= std_logic_vector(A srl conv_integer(B));
+        loopforshift: for i in 0 to tmp loop
+              SHR_ONE port map (tmp2,tmp2);
+        end loop;
+        Shift <= tmp2;
+end process
 ----------------------------------------
 end gate_level;
 
