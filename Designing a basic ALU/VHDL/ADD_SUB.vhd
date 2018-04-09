0a6224abbcdb7ce6d53df1a6da05769969e9b5a8
Maor Assayag <maorassayag16@gmail.com>
2018-04-09 07:51:31 +0300

    refactor 1.6

--- Designing a basic ALU/VHDL/ADD_SUB.vhd
+++ Designing a basic ALU/VHDL/ADD_SUB.vhd
@@ -65,12 +65,12 @@ begin
       if (addORsub = '1') then
         FLAG <= FLAGS; -- will assign at the end of process
         FLAGS(0) := '1'; -- is A=B ?
-        wait on tempSUM2;
+        wait on SUM;
         eachBit: for i in 0 to (N-1) loop
-          FLAGS(0) := (FLAGS(0) AND (NOT tempSUM2(i)));
+          FLAGS(0) := (FLAGS(0) AND (NOT SUM(i)));
         end loop;
         FLAGS(1) := NOT FLAGS(0); -- A!=B
-        FLAGS(2) := NOT tempSUM2(N-1);--A >= B if tempSUM(N-1)=0 then
+        FLAGS(2) := NOT SUM(N-1);--A >= B if tempSUM(N-1)=0 then
         FLAGS(3) := FLAGS(2) AND FLAGS(1); -- A>B if A>=B & A!=B
         FLAGS(4) := NOT FLAGS(3);-- A<=B if !(A>B)
         FLAGS(5) := FLAGS(4) AND FLAGS(1); -- A<B if (A<=B & A!=B)
