7bc169314cc04913bc34da9a46d841ab5a5b8fc3
refael <rapooil120@gmail.com>
2018-04-10 14:08:26 +0300

    MUL

---
+++ Designing a basic ALU/TB/TestBench_MUL.vhd
@@ -0,0 +1,50 @@
+-- ====================================================================
+--
+--	File Name:		TestBench_MUL.vhd
+--	Description: test bench for MUL opp
+--
+--
+--	Date:			10/04/2018
+--	Designer:		Maor Assayag, Refael Shetrit
+--
+-- ====================================================================
+
+LIBRARY ieee;
+use ieee.std_logic_1164.ALL;
+use ieee.numeric_std.all;
+
+entity Testbench_MUL is
+end Testbench_MUL;
+
+architecture behavior of Testbench_MUL is
+
+ -- Component Declaration
+ component MUL
+    generic (N: integer := 8 ); --defualt value for N is 8
+    port(
+        A :     in signed((N-1) downto 0);
+        B :     in signed((N-1) downto 0);
+        HI :   out signed((N-1) downto 0);
+        LO : out signed((N-1) downto 0)
+     );
+  end component;
+
+ constant N : integer := 8;
+ signal x, y, HI,LO : signed((N-1) downto 0) ;
+
+begin
+----------------------------------------
+  uut :  MUL  generic map(N)
+    port map (A => x,B => y,HI => HI,LO => LO);
+
+  stim: process
+  begin
+    wait for 100 ns;
+    x <= "00000000", "00000001" after 50 ns, "00001100" after 100 ns,"00001111" after 150 ns;
+    y <= "00000010", "00000011" after 50 ns, "00000010" after 100 ns,"00010000" after 150 ns;
+    wait for 50 ns;
+  end process stim;
+----------------------------------------
+end behavior;
+
+--EndOfFile
---
+++ Designing a basic ALU/VHDL/MUL.vhd
@@ -0,0 +1,44 @@
+-- ====================================================================
+--
+--	File Name:		MUL.vhd
+--	Description:	MUL command, currently support N bit's
+--
+--
+--	Date:			02/04/2018
+--	Designer:		Maor Assayag, Refael Shetrit
+--
+-- TODO : check test bench
+-- ====================================================================
+
+-- libraries decleration
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+ -- entity Definition
+entity MUL is
+    generic(N: integer := 8); --defualt value for N is 8
+    Port(
+       A :     in signed((N-1) downto 0);
+       B :     in signed((N-1) downto 0);
+       HI :   out signed((N-1) downto 0);
+       LO : out signed((N-1) downto 0)
+    );
+end MUL;
+
+ -- Architecture Definition
+architecture gate_level of MUL is
+
+signal tmp : signed (2*N - 1 downto 0);
+begin
+  ----------------------------------------
+  MUL_proc : process(A,B)
+  begin
+    tmp <= A*B;
+    HI <= tmp (2*N - 1 to N);
+    LO <= tmp (N-1 to 0);
+  end process;
+----------------------------------------
+end gate_level;
+
+--EndOfFile
