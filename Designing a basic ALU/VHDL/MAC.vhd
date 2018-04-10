e022ec06b3a6983630bac3dc985d4226f9c1fcf3
refael <rapooil120@gmail.com>
2018-04-10 16:48:14 +0300

    mac

--- Designing a basic ALU/VHDL/MAC.vhd
+++ Designing a basic ALU/VHDL/MAC.vhd
@@ -15,6 +15,7 @@ library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 
+
  -- entity Definition
 entity MAC is
     generic(N: integer := 8); --defualt value for N is 8
@@ -30,6 +31,16 @@ end MAC;
 
  -- Architecture Definition
 architecture gate_level of MAC is
+  component ADD_SUB
+     generic (N: integer := 8 ); --defualt value for N is 8
+     port(
+       addORsub :   in std_logic;
+       FLAG : inout std_logic_vector(5 downto 0);
+       A :     in signed ((N-1) downto 0);
+       B :     in signed ((N-1) downto 0);
+       SUM :   out signed ((N-1) downto 0)
+      );
+   end component;
   component MUL
      generic (N: integer := 8 ); --defualt value for N is 8
      port(
@@ -39,32 +50,26 @@ architecture gate_level of MAC is
          LO : out signed((N-1) downto 0)
       );
    end component;
-   component ADD_SUB
-      generic (N: integer := 8 ); --defualt value for N is 8
-      port(
-        addORsub :   in std_logic;
-        FLAG : inout std_logic_vector(5 downto 0);
-        A :     in signed ((N-1) downto 0);
-        B :     in signed ((N-1) downto 0);
-        SUM :   out signed ((N-1) downto 0)
-       );
-    end component;
+
     signal MULHI,MULLO : signed((N-1) downto 0) ;
     signal MULT,MAC,SUM: signed((2*N-1) downto 0) ;
+    signal FLAGs: std_logic_vector (5 downto 0) := "000000";
 begin
   ----------------------------------------
   MULFUN :  MUL  generic map(N)
     port map (A,B,MULT (2*N-1 downto N),MULT (N-1 downto 0));
   -- MULT (2*N-1 downto N) <= MULHI;
   -- MULT (N-1 downto 0) <= MULLO;
-MAC_proc : process(MACHI,MACLO,MULT)
+MAC_proc : process
 begin
+  wait on MACHI;
+  wait on MACLO;
   MAC (2*N-1 downto N) <= MACHI;
   MAC (N-1 downto 0) <= MACLO;
 end process;
 
   ADDFUN :  ADD_SUB  generic map(2*N)
-    port map ('0', "000000", MULT,MAC,SUM);
+    port map ('0', FLAGs, MULT,MAC,SUM);
 MAC_proc2 : process(MAC,SUM)
 begin
   HI <= SUM(2*N-1 downto N);
